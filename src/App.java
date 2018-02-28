0842f0d7369b06e7ba601cb3f164c280e396ac96
Maxim <31003295+OdareNNbI@users.noreply.github.com>
2018-02-28 23:14:01 +0300

    Add addition to Laba1
    Add Javadoc
    Add class with constants

--- src/App.java
+++ src/App.java
@@ -1,84 +1,130 @@
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+
+/** 
+ *The class creates a window with 
+ *the ability to copy information
+ * 
+ * @version 1.0
+ * @author Maxim
+ * @since 2018-02-28
+ */
 
 public class App {
+	/**Property - to copy Text from 1 field to 2 field.*/
+	private Button buttonCopy1to2;
+	/** @see App#copy1to2*/
+	private Button buttonCopy2to3;
+	/**Property - field for input text.*/
+	private Text firstText;
+	/**@see App#firstText*/
+	private Text secondText;
+	/**@see App#firstText*/
+	private Text thirdText;
+	/**Constructor - creating a new object.*/
+	public App() {
+		Display display = new Display();
 
-final static int sizeX = 300;
-final static int sizeY = 150;
-final static int sizeTextX = 70;
-final static int sizeTextY = 15;
-final static int sizeButtonX = 70;
-final static int sizeButtonY = 30;
-final static int distance = 15;
-final static int posYForText = 10;
-
-
-public static void main(String args[]) {
-	Display display = new Display();
-	final Shell shell = new Shell(display);
-	shell.setSize(sizeX, sizeY);
-	GridLayout layout  =  new GridLayout();
-	layout.numColumns = 3;
-	shell.setLayout(layout);
-	
-	Text text1 = new Text(shell,SWT.CENTER);
-	text1.setSize(sizeTextX,sizeTextY);
-	text1.setLocation(distance, posYForText);
-
-	Text text2 = new Text(shell,SWT.CENTER);
-	text2.setSize(sizeTextX,sizeTextY);
-	text2.setLocation(text1.getLocation().x + sizeTextX + distance, posYForText);
-	
-	Text text3 = new Text(shell,SWT.CENTER);
-	text3.setSize(sizeTextX,sizeTextY);
-	text3.setLocation(text2.getLocation().x + sizeTextX + distance, posYForText);
-	
-	
-	
-
-	Button copy1to2 = new Button(shell,SWT.CENTER);
-	copy1to2.setSize(sizeButtonX, sizeButtonY);
-	copy1to2.setLocation(text1.getLocation().x, text1.getLocation().y + distance * 2);
-	copy1to2.setText("Copy 1 to 2");
-	
-	Button copy2to3 = new Button(shell,SWT.CENTER);
-	copy2to3.setSize(sizeButtonX, sizeButtonY);
-	copy2to3.setLocation(text2.getLocation().x, text2.getLocation().y + distance * 2);
-	copy2to3.setText("Copy 2 in 3");
-	
-	
-	
+		final Shell shell = new Shell(display);
 		
-	
+		shell.setText("Copy manager");
+		shell.setSize(CONSTANTS.WINDOW_WIGTH,CONSTANTS.WINDOW_HEIGHT);
+		
+		GridLayout gridLayout = new GridLayout(3, true);
+		gridLayout.makeColumnsEqualWidth = false;
+		shell.setLayout(gridLayout);
+		
+		
+	    Composite firstColomnComposite = new Composite(shell,SWT.NONE);
+	    
+		RowLayout firstColomnLayout = new RowLayout();
+		firstColomnLayout.type = SWT.VERTICAL;
+		firstColomnLayout.spacing = CONSTANTS.SPACING_BEETWEN_ELEMENTS;
+		firstColomnComposite.setLayout(firstColomnLayout);
+		
+		firstText = new Text(firstColomnComposite, SWT.NONE);
+		firstText.setLayoutData(new RowData(CONSTANTS.TEXT_WEIGTH,CONSTANTS.TEXT_HEIGHT));
+		
+		buttonCopy1to2 = new Button(firstColomnComposite, SWT.PUSH);
+		buttonCopy1to2.setLayoutData(new RowData(CONSTANTS.BUTTON_WEIGTH,CONSTANTS.BUTTON_HEIGHT));
+		buttonCopy1to2.setText("Copy 1 to 2");
+		
+		
+		
+		Composite secondColomnComposite = new Composite(shell,SWT.NONE);
+		
+		RowLayout secondColomnLayout = new RowLayout();
+		secondColomnLayout.type = SWT.VERTICAL;
+		secondColomnLayout.spacing = CONSTANTS.SPACING_BEETWEN_ELEMENTS;
+		secondColomnComposite.setLayout(secondColomnLayout);
+		
+		secondText = new Text(secondColomnComposite, SWT.NONE);
+		secondText.setLayoutData(new RowData(CONSTANTS.TEXT_WEIGTH,CONSTANTS.TEXT_HEIGHT));	
+		
+		buttonCopy2to3 = new Button(secondColomnComposite, SWT.PUSH);
+		buttonCopy2to3.setLayoutData(new RowData(CONSTANTS.BUTTON_WEIGTH,CONSTANTS.BUTTON_HEIGHT));
+		buttonCopy2to3.setText("Copy 2 to 3");
+		
+		
+		
+		Composite thirdColomnComposite = new Composite(shell,SWT.NONE);
+		
+		RowLayout thirdColomnLayout = new RowLayout();
+		thirdColomnLayout.type = SWT.VERTICAL;
+		thirdColomnLayout.spacing = CONSTANTS.SPACING_BEETWEN_ELEMENTS;
+		thirdColomnLayout.marginBottom = 42;
+		thirdColomnComposite.setLayout(thirdColomnLayout);
+		thirdText = new Text(thirdColomnComposite, SWT.NONE);
+		thirdText.setLayoutData(new RowData(CONSTANTS.TEXT_WEIGTH,CONSTANTS.TEXT_HEIGHT));
+		
+		/**@see App#setButtonEvent(Button, Text, Text)*/
+		setButtonEvent(buttonCopy1to2, firstText, secondText);
+		setButtonEvent(buttonCopy2to3, secondText, thirdText);
+		
+		shell.open();
 
-	copy1to2.addListener(SWT.Selection, new Listener() {
-		@Override
-		public void handleEvent(Event event)
-		{
-			text2.setText(text1.getText());
-		}		
-	});
-	
-	copy2to3.addListener(SWT.Selection, new Listener() {
-		@Override
-		public void handleEvent(Event event)
-		{
-			text3.setText(text2.getText());
-		}		
-	});
-	
-	
-	
-	
-	
-	
-	
-	shell.open();
-	while (!shell.isDisposed()) {
-		if (!display.readAndDispatch())
-			display.sleep();
+	    while (!shell.isDisposed()) {
+	    	if (!display.readAndDispatch()) {
+	                display.sleep();
+	        }
+	    } 
+
+		display.dispose();
+
 	}
-	display.dispose();
-}
+	
+	/** Assigns an event to the button.
+     * @param btn - that button which assigned event.
+     * @param from - text from where the information was copied.
+     * @param to - text in which the copied information will be.
+     * @return Nothing.
+    */
+	private void setButtonEvent(Button btn,Text from,Text to)	{
+		btn.addListener(SWT.Selection,new Listener(){
+			@Override
+			public void handleEvent(Event e)
+			{
+				to.setText(from.getText());
+			}
+		});
+	}
+	
+	/** The main method where the window object is created
+	 * @param args - arguments from command line.
+	 * @return Nothing.
+	 */
+	public static void main(final String[] args) {
+		new App();
+	}
+
 }
---
+++ src/CONSTANTS.java
@@ -0,0 +1,10 @@
+
+public class CONSTANTS {
+	public static final int WINDOW_WIGTH = 250;
+	public static final int WINDOW_HEIGHT = 150;
+	public static final int BUTTON_WEIGTH = 65;
+	public static final int BUTTON_HEIGHT = 30;
+	public static final int TEXT_HEIGHT = 15;
+	public static final int TEXT_WEIGTH = 60;
+	public static final int SPACING_BEETWEN_ELEMENTS = 10;
+}
